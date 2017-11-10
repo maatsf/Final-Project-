647714ab12c601d5c6a1fc4cfb9c26aab209c499
olejacobb <olejacobb@hotmail.com>
2017-11-10 13:39:49 +0100

    :sunglasses:

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-var indeksSynligBilde = 0; var indeksNesteBilde;
+var indeksSynligBilde = 0;
+var indeksNesteBilde;
 var tid = 3000;
 // En array med alle bildefilene
-var bildefiler = ["bilde1.jpg", "bilde2.jpg", "bilde3.png", "bilde4.png"];
+var bildefiler = ["bilde1.jpg", "bilde2.jpg", "bilde3.jpg", "bilde4.jpg"];
 
 // Henter alle elementer med klassen .bilde
 var bilder = document.querySelectorAll(".bilde");
 // Legger til bakgrunnsbildene,
-// og setter display: none på alle bortsett fra første bilde for (var i = 0; i < bilder.length; i++) {
+// og setter display: none på alle bortsett fra første bilde
+for (var i = 0; i < bilder.length; i++) {
 // Legger til bildefilene som bakgrunnsbilder
-bilder[i].style.backgroundImage = "url(bilder/" + bildefiler[i] + ")";
+bilder[i].style.backgroundImage = "url(media/" + bildefiler[i] + ")";
+
 // Hvis bildet ikke er det første bildet (med indeks 0)
 if (i != 0) {
-// Setter display til none (usynlig) bilder[i].style.display = "none";
+// Setter display til none (usynlig)
+bilder[i].style.display = "none";
 }
+}
+
+setTimeout(flytt, tid);
 
 function flytt() {
   if (indeksSynligBilde === bilder.length - 1) {
